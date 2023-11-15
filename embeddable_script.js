04742140302dba436ee4bf414cbbc3d84aba3640
MutableAI <mutableai@github.com>
2023-11-15 18:16:23 +0000

    [code modification] functions

--- embeddable_script.js
+++ embeddable_script.js
@@ -16,25 +16,51 @@ document.addEventListener("DOMContentLoaded", () => {
         <div id="chat-window"></div>
         <input id="user-input" type="text" />
         <button id="submit-button">Submit</button>
+        <button id="check-report">Check Report Status</button>
+        <button id="legal-info">Legal Information</button>
     `;
 
     botContainer.appendChild(botUI);
 
+    // Create reference for new elements in the UI
+    let checkReportButton = document.getElementById("check-report");
+    let legalInfoButton = document.getElementById("legal-info");
+    
     let submitButton = document.getElementById("submit-button");
     let userInput = document.getElementById("user-input");
     let chatWindow = document.getElementById("chat-window");
     
+    // Function for dealing with bot response
+    const handleResponse = (response) => {
+        let botResponse = document.createElement("p");
+        botResponse.innerText = "Justice Sentinel: " + response;
+        chatWindow.appendChild(botResponse);
+
+        // Clear the user input field
+        userInput.value = "";        
+    }
+    
+    // Submit user query to bot
     submitButton.onclick = () => {
         let userText = userInput.value;
-        aiBot.process_text(userText)
-            .then(response => {
-                let botResponse = document.createElement("p");
-                botResponse.innerText = "Justice Sentinel: " + response;
-                chatWindow.appendChild(botResponse);
-
-                // Clear the user input field
-                userInput.value = "";
-            })
+        aiBot.converse(userText)
+            .then(response => handleResponse(response))
+            .catch(err => console.error(err));
+    };
+    
+    // Submit report status check request to bot
+    checkReportButton.onclick = () => {
+        let userText = userInput.value;
+        aiBot.checkReportStatus(userText)
+            .then(response => handleResponse(response))
+            .catch(err => console.error(err));
+    };
+    
+    // Submit legal info request to bot
+    legalInfoButton.onclick = () => {
+        let userText = userInput.value;
+        aiBot.provideLegalInfo(userText)
+            .then(response => handleResponse(response))
             .catch(err => console.error(err));
     };
 });
